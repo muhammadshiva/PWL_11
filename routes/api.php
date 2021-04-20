4d5b2c771f31a16cebf22b7034906dd2c9b481de
muhammadshiva <shivamuhammad07@gmail.com>
2021-04-20 10:58:03 +0700

    feat(routes): Initialize logout to api.php

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
