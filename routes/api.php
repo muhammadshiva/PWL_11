2e5ea332498814f5ab2905e40757e235c26e7a9d
muhammadshiva <shivamuhammad07@gmail.com>
2021-04-20 11:43:07 +0700

    feat(routes): Add todos resources

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TodoController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,4 +22,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/todos', TodoController::class);
 });
