f2482786de65adc2cd94a591dc91264dab1f90a5
muhammadshiva <shivamuhammad07@gmail.com>
2021-04-20 11:53:01 +0700

    fix(routes): Fix api route

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('/todos', TodoController::class);
